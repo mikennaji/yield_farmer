8f8284c25dad11158f8a30edb77187c1a05fb870
mikennaji <tobienaji@gmail.com>
2020-08-21 19:42:34 -0400

    plan

--- lib/yield_farmer.rb
+++ lib/yield_farmer.rb
@@ -1,5 +1,4 @@
-require 'nokogiri'
-require 'open-uri'
+
 
 require_relative './yield_farmer/version'
 require_relative './yield_farmer/controller'
--- lib/yield_farmer/controller.rb
+++ lib/yield_farmer/controller.rb
@@ -1,11 +1,45 @@
 class YieldFarmer::CLI
 
+    attr_accessor :input
+
 def call
     hello
+    menu
 end 
 
 def hello
     puts"hello" 
 end
 
+
+
+
+def menu
+    puts "Welcome farmer!"
+    puts "do you want to borrow or lend"
+    input = gets.strip
+    while input!="lend" || input !="borrow"
+      puts "Please enter a valid entry"
+       input = gets.chomp
+      puts input
+      break if input=="lend" || input =="borrow"
+    end
+    if input == "lend"
+        puts"Which coin would you like to farm today?"
+         puts "List of coins"
+         puts "if coin is selected"
+         puts "view rates for coin and exchange listed on"
+         puts "based on selection send person to website to lend"
+    elsif input== "borrow"
+         puts"list of coins"
+         puts "if coin is selcted"
+         puts "view rates for coin and exchange list on"
+         puts "based on selection send person to website to lend"
+ 
+     else
+         puts "enter a valid entry"
+     end
 end
+
+
+end 
