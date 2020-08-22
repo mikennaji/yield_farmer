a596dbe6286fba2f3ea7960f28b4199a1354a777
mikennaji <tobienaji@gmail.com>
2020-08-22 15:08:34 -0400

    add logic

--- lib/yield_farmer/scraper.rb
+++ lib/yield_farmer/scraper.rb
@@ -4,11 +4,24 @@ class YieldFarmer::Scraper
 
  
 
+
 def url
    html =open("https://defirate.com/lend/")
     doc = Nokogiri::HTML(html)
-    return doc.css("div.symbol-cell").text
-    
+    rates =[]
+    exchanges = []
+    coins =[]
+    doc.css(".token-cell span.name").each do |exchange|
+        exchanges << exchange.text 
+    end 
+    doc.css(".rate-cell").each do |rate|
+       rates<< rate.text
+    end
+    doc.css("div.symbol-content span.name").each do |coin|
+        coins<<coin.text 
+    end
+    return rates, exchanges, coins  
+     
 end
 
 end 
